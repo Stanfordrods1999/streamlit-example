6f6a617b865f8e99035bd7f86b59af7cb9b1f8ef
Stanny <127314109+Stanfordrods1999@users.noreply.github.com>
2023-07-11 07:23:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,27 +5,18 @@ import pandas as pd
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import threading
+from streamlit_javascript import st_javascript
 
-lock = threading.Lock()
-img_container = {"img": None}
-frame_rate = 1
-is_true=False
+js_code = """window.addEventListener('beforeunload', function(event) {
+  // Check if the user is closing the tab
+  if (!event.clientY) {
+    // User is closing the tab
+    return true;
+    // Perform any necessary actions here
+    // ...
+  }
+});"""
 
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-    with lock:
-        img_container["img"] = img
-    return frame
-  
-ctx = webrtc_streamer(key="example", video_frame_callback=video_frame_callback,
-                       media_stream_constraints={"video": {"frameRate": {"ideal": frame_rate}}},
-    video_html_attrs=({
-        "controls": False,
-        "autoPlay": True,
-    }))
-while ctx.state.playing:
-  with lock:
-    img = img_container["img"]
-    if img is not None:
-      is_true=True
-  print(frame_rate,is_true)  
+return_value = st_javascript(js_code)
+
+st.markdown(return_value)
